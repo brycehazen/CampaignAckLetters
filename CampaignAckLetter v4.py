fe854e73b9e2f5bd1e6c9f3c08a18b0deb083618
brycehazen <39276420+brycehazen@users.noreply.github.com>
2023-12-19 16:46:07 -0500

    Update CampaignAckLetter v4.py

--- CampaignAckLetter v4.py
+++ CampaignAckLetter v4.py
@@ -47,22 +47,34 @@ commonTitles = ['Mrs.', 'Mr.', 'Ms.', 'Miss','Sr.','Sra.', 'Señor']
 #     df = df.drop_duplicates(subset=['Gf_CnBio_ID'])
 
 # Define a function to remove data based on conditions
-def remove_data_based_on_conditions(row):
-    # Check if 'Gf_CnBio_First_Name' is equal to 'Gf_CnSpSpBio_First_Name' and remove data if True
-    if row['Gf_CnBio_First_Name'] == row['Gf_CnSpSpBio_First_Name']:
-        row['Gf_CnSpSpBio_Gender'] = ''
-        row['Gf_CnSpSpBio_Title_1'] = ''
-        row['Gf_CnSpSpBio_First_Name'] = ''
-        row['Gf_CnSpSpBio_Last_Name'] = ''
-
-    # Check if 'Gf_CnSpSpBio_Inactive' or 'Gf_CnSpSpBio_Deceased' is 'Yes' and remove data if True
-    if row['Gf_CnSpSpBio_Inactive'] == 'Yes' or row['Gf_CnSpSpBio_Deceased'] == 'Yes' or ['Gf_CnBio_Marital_status'] == 'Widowed' or ['Gf_CnBio_Marital_status'] == 'Divorced':
-        row['Gf_CnSpSpBio_Gender'] = ''
-        row['Gf_CnSpSpBio_Title_1'] = ''
-        row['Gf_CnSpSpBio_First_Name'] = ''
-        row['Gf_CnSpSpBio_Last_Name'] = ''
+def remove_data_based_on_condition1(row):
+    # Check if 'CnBio_First_Name' is equal to 'CnSpSpBio_First_Name' and remove data if True
+    if row['CnBio_First_Name'] == row['CnSpSpBio_First_Name']:
+        row['CnSpSpBio_Gender'] = None
+        row['CnSpSpBio_Title_1'] = None
+        row['CnSpSpBio_First_Name'] = None
+        row['CnSpSpBio_Last_Name'] = None
+        row['CnBio_Marital_status'] = 'WidSinDiv_0'
 
     return row
+
+# Apply the function to your DataFrame
+df = df.apply(remove_data_based_on_condition1, axis=1)
+
+# Define a function to remove data based on conditions
+def remove_data_based_on_condition2(row):
+    # Check if 'CnSpSpBio_Inactive' or 'CnSpSpBio_Deceased' is 'Yes' and remove data if True
+    if row['CnSpSpBio_Inactive'] == 'Yes' or row['CnSpSpBio_Deceased'] == 'Yes' or row['CnBio_Marital_status'] == 'Widowed' or row['CnBio_Marital_status'] == 'Divorced':
+        row['CnSpSpBio_Gender'] = None
+        row['CnSpSpBio_Title_1'] = None
+        row['CnSpSpBio_First_Name'] = None
+        row['CnSpSpBio_Last_Name'] = None
+        row['CnBio_Marital_status'] = 'WidSinDiv_0'
+
+    return row
+
+# Apply the function to your DataFrame
+df = df.apply(remove_data_based_on_condition2, axis=1)
 
 # Apply the function to your DataFrame
 df = df.apply(remove_data_based_on_conditions, axis=1)
